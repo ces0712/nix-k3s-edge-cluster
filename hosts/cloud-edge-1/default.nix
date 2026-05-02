aff70d60bdb0f53bef0b5f0bd45f798c8d455f49
ces0712 <cesaraugusto0712@gmail.com>
2026-05-01 23:13:54 -0300

    fix(update): improve clean up process to fix memory problems

--- hosts/cloud-edge-1/default.nix
+++ hosts/cloud-edge-1/default.nix
@@ -26,6 +26,7 @@
   boot.loader.systemd-boot = {
     enable = true;
     editor = false;
+    configurationLimit = 3;
   };
   boot.loader.efi = {
     canTouchEfiVariables = true;
--- modules/common-base.nix
+++ modules/common-base.nix
@@ -59,7 +59,7 @@ in {
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 3d";
     };
     optimise = {
       automatic = true;
