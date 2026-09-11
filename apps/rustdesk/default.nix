c1c194ee599a8fc373f3c4efb08561391c9d8119
ces0712 <cesaraugusto0712@gmail.com>
2026-09-11 19:37:56 -0300

    feat: searxng features

--- apps/rustdesk/default.nix
+++ apps/rustdesk/default.nix
@@ -49,6 +49,7 @@ in
           };
           spec = {
             replicas = 1;
+            strategy.type = "Recreate";
             selector.matchLabels = {
               "app.kubernetes.io/name" = "rustdesk";
             };
